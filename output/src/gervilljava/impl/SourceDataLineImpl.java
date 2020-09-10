6209c00ac2f196bb640a326babd76bbe2680ad80
Hector Ricardo Mendez Sordia <hectorricardomendez@gmail.com>
2020-09-09 20:36:28 -0500

    Fix spacing

--- output/src/gervilljava/impl/SourceDataLineImpl.java
+++ output/src/gervilljava/impl/SourceDataLineImpl.java
@@ -6,143 +6,143 @@ import javax.sound.sampled.LineUnavailableException;
 
 public class SourceDataLineImpl implements SourceDataLine {
 
-	private final javax.sound.sampled.SourceDataLine realLine;
+    private final javax.sound.sampled.SourceDataLine realLine;
 
-	public SourceDataLineImpl(AudioFormat format) {
-		try {
-			realLine = javax.sound.sampled.AudioSystem.getSourceDataLine(null);
-		} catch (LineUnavailableException e) {
-			throw new RuntimeException(e.getMessage());
-		}
-	}
+    public SourceDataLineImpl(AudioFormat format) {
+        try {
+            realLine = javax.sound.sampled.AudioSystem.getSourceDataLine(null);
+        } catch (LineUnavailableException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
-	@Override
-	public void open(AudioFormat format, int bufferSize) {
-		try {
-			realLine.open(new javax.sound.sampled.AudioFormat(format.getSampleRate(), 16, format.getChannels(), true, false), bufferSize);
-		} catch (LineUnavailableException e) {
-			throw new RuntimeException(e.getMessage());
-		}
-	}
+    @Override
+    public void open(AudioFormat format, int bufferSize) {
+        try {
+            realLine.open(new javax.sound.sampled.AudioFormat(format.getSampleRate(), 16, format.getChannels(), true, false), bufferSize);
+        } catch (LineUnavailableException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
-	@Override
-	public void open(AudioFormat format) {
-	}
+    @Override
+    public void open(AudioFormat format) {
+    }
 
-	@Override
-	public int write(byte[] b, int off, int len) {
-		return realLine.write(b, off, len);
-	}
+    @Override
+    public int write(byte[] b, int off, int len) {
+        return realLine.write(b, off, len);
+    }
 
-	@Override
-	public void drain() {
-		realLine.drain();
-	}
+    @Override
+    public void drain() {
+        realLine.drain();
+    }
 
-	@Override
-	public void flush() {
-		realLine.flush();
-	}
+    @Override
+    public void flush() {
+        realLine.flush();
+    }
 
-	@Override
-	public void start() {
-		realLine.start();
-	}
+    @Override
+    public void start() {
+        realLine.start();
+    }
 
-	@Override
-	public void stop() {
-		realLine.stop();
-	}
+    @Override
+    public void stop() {
+        realLine.stop();
+    }
 
-	@Override
-	public boolean isRunning() {
-		return realLine.isRunning();
-	}
+    @Override
+    public boolean isRunning() {
+        return realLine.isRunning();
+    }
 
-	@Override
-	public boolean isActive() {
-		return realLine.isActive();
-	}
+    @Override
+    public boolean isActive() {
+        return realLine.isActive();
+    }
 
-	@Override
-	public AudioFormat getFormat() {
-		return null;
-	}
+    @Override
+    public AudioFormat getFormat() {
+        return null;
+    }
 
-	@Override
-	public int getBufferSize() {
-		return realLine.getBufferSize();
-	}
+    @Override
+    public int getBufferSize() {
+        return realLine.getBufferSize();
+    }
 
-	@Override
-	public int available() {
-		return realLine.available();
-	}
+    @Override
+    public int available() {
+        return realLine.available();
+    }
 
-	@Override
-	public int getFramePosition() {
-		return realLine.getFramePosition();
-	}
+    @Override
+    public int getFramePosition() {
+        return realLine.getFramePosition();
+    }
 
-	@Override
-	public long getLongFramePosition() {
-		return realLine.getLongFramePosition();
-	}
+    @Override
+    public long getLongFramePosition() {
+        return realLine.getLongFramePosition();
+    }
 
-	@Override
-	public long getMicrosecondPosition() {
-		return realLine.getMicrosecondPosition();
-	}
+    @Override
+    public long getMicrosecondPosition() {
+        return realLine.getMicrosecondPosition();
+    }
 
-	@Override
-	public float getLevel() {
-		return realLine.getLevel();
-	}
+    @Override
+    public float getLevel() {
+        return realLine.getLevel();
+    }
 
-	@Override
-	public Line.Info getLineInfo() {
-		return null;
-	}
+    @Override
+    public Line.Info getLineInfo() {
+        return null;
+    }
 
-	@Override
-	public void open() {
-		try {
-			realLine.open();
-		} catch (LineUnavailableException e) {
-			throw new RuntimeException(e.getMessage());
-		}
-	}
+    @Override
+    public void open() {
+        try {
+            realLine.open();
+        } catch (LineUnavailableException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
-	@Override
-	public void close() {
-		realLine.close();
-	}
+    @Override
+    public void close() {
+        realLine.close();
+    }
 
-	@Override
-	public boolean isOpen() {
-		return realLine.isOpen();
-	}
+    @Override
+    public boolean isOpen() {
+        return realLine.isOpen();
+    }
 
-	@Override
-	public Control[] getControls() {
-		return null;
-	}
+    @Override
+    public Control[] getControls() {
+        return null;
+    }
 
-	@Override
-	public boolean isControlSupported(Control.Type control) {
-		return false;
-	}
+    @Override
+    public boolean isControlSupported(Control.Type control) {
+        return false;
+    }
 
-	@Override
-	public Control getControl(Control.Type control) {
-		return null;
-	}
+    @Override
+    public Control getControl(Control.Type control) {
+        return null;
+    }
 
-	@Override
-	public void addLineListener(LineListener listener) {
-	}
+    @Override
+    public void addLineListener(LineListener listener) {
+    }
 
-	@Override
-	public void removeLineListener(LineListener listener) {
-	}
+    @Override
+    public void removeLineListener(LineListener listener) {
+    }
 }
--- output/src/gervilljava/main/Main.java
+++ output/src/gervilljava/main/Main.java
@@ -10,44 +10,44 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		// Step 1: Select random soundbank
-		Soundbank soundbank;
+    public static void main(String[] args) throws IOException {
+        // Step 1: Select random soundbank
+        Soundbank soundbank;
 
-		switch (ThreadLocalRandom.current().nextInt(3)) {
-		case 0:
-			soundbank = new SF2Soundbank(new File("assets/gm.sf2"));
-			break;
-		case 1:
-			soundbank = new DLSSoundbank(new File("assets/gm.dls"));
-			break;
-		case 2:
-			soundbank = null;
-			break;
-		default:
-			throw new RuntimeException("WTF just happened");
-		}
+        switch (ThreadLocalRandom.current().nextInt(3)) {
+            case 0:
+                soundbank = new SF2Soundbank(new File("assets/gm.sf2"));
+                break;
+            case 1:
+                soundbank = new DLSSoundbank(new File("assets/gm.dls"));
+                break;
+            case 2:
+                soundbank = null;
+                break;
+            default:
+                throw new RuntimeException("WTF just happened");
+        }
 
-		// Step 2. Set up synthesizer
-		final SynthesizerPlayer player = new SynthesizerPlayer();
-		player.init(soundbank);
+        // Step 2. Set up synthesizer
+        final SynthesizerPlayer player = new SynthesizerPlayer();
+        player.init(soundbank);
 
-		System.out.println(player.getInstrumentInfo());
+        System.out.println(player.getInstrumentInfo());
 
-		// Step 3: Play something
-		if (player.readyToStartPlaying()) { // will always be true
-			final String[] instrumentInfo = player.sample1();
+        // Step 3: Play something
+        if (player.readyToStartPlaying()) { // will always be true
+            final String[] instrumentInfo = player.sample1();
 
-			// Print details
-			System.out.println(soundbank == null ? "Default soundbank" : soundbank.getClass().getSimpleName());
-			System.out.println("--------------------------");
-			System.out.println(instrumentInfo[0]);
-			System.out.println(instrumentInfo[1]);
+            // Print details
+            System.out.println(soundbank == null ? "Default soundbank" : soundbank.getClass().getSimpleName());
+            System.out.println("--------------------------");
+            System.out.println(instrumentInfo[0]);
+            System.out.println(instrumentInfo[1]);
 
-			player.sample2();
-		}
+            player.sample2();
+        }
 
-		// Step 4. Close synthesizer
-		player.close();
-	}
+        // Step 4. Close synthesizer
+        player.close();
+    }
 }
